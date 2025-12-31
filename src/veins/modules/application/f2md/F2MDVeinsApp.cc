3e6ba0e1e4c6d7fddcc3b4e0ee2c1abde9278d14
Omykronn <omykronn.player@gmail.com>
2025-12-31 12:52:49 +0100

    If you don't rebuild, it won't do anything
    This reverts commit 1294d8da15757f253dd5fa2b65888af83fdaa2fc.

--- src/veins/modules/application/f2md/F2MDVeinsApp.cc
+++ src/veins/modules/application/f2md/F2MDVeinsApp.cc
@@ -625,8 +625,6 @@ void JosephVeinsApp::treatAttackFlags()
             traciVehicle->setColor(TraCIColor(0, 0, 0, 255));
         }
         else {
-            std::cout << "myAttackType = " << myAttackType << '\n';
-
             if (myAttackType == attackTypes::Attacks::Target) {
                 traciVehicle->setColor(TraCIColor(255, 0, 255, 255));
             }
